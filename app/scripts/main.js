f20e37a3f73efe473bb8d6c8f5f060d0443f0ec2
Pieter Feenstra <feenstra48@hotmail.com>
2017-10-13 22:47:48 +0200

    First 3 exercises

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,40 @@
+
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Vue JS',
     pros: 'The Progressive JavaScript Framework : Approachable, Versatile, Performant'
   }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+
+var app3 = new Vue({
+  el:'#app-3',
+  data:{
+    seen: true,
+    message: 'HTML5'
+  }
+})
+
+var todos = [
+  { text: 'Learn JavaScript'},
+  {text : 'learn Vue'},
+  { text: 'Use Bootstrap'}
+];
+
+
+var app4 = new Vue({
+  el: '#app-4',
+  data : {
+    todos,
+    message: 'ToDO'
+  }
+
+
 })
